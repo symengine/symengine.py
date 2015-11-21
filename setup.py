59a10675517197f1c05c1252330948be6d89faaf
Sumith <sumith1896@gmail.com>
2015-11-21 22:00:00 +0530

    Check Python version

--- setup.py
+++ setup.py
@@ -3,6 +3,11 @@ from os import getenv, path
 import subprocess
 import sys
 
+# Make sure the system has the right Python version.
+if sys.version_info[:2] < (2, 6):
+    print("SymEngine requires Python 2.6 or newer. Python %d.%d detected" % sys.version_info[:2])
+    sys.exit(-1)
+
 # use setuptools by default as per the official advice at:
 # packaging.python.org/en/latest/current.html#packaging-tool-recommendations
 use_setuptools = True
