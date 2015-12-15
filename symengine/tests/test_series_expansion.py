4e1ea8ceafc10e8cc69123d0fe8c6ff643927ed9
Isuru Fernando <isuruf@gmail.com>
2015-12-15 23:28:28 +0530

    Test series only if piranha is installed

--- symengine/tests/test_series_expansion.py
+++ symengine/tests/test_series_expansion.py
@@ -1,9 +1,11 @@
 from symengine.utilities import raises
 
 from sympy import Symbol, Integer, sin, cos, exp, sqrt, E
-from symengine.lib.symengine_wrapper import series, Symbol as SESymbol
+from symengine.lib.symengine_wrapper import series, have_piranha, Symbol as SESymbol
 
 def test_series_expansion():
+    if not have_piranha:
+        return
     x = Symbol('x')
     ex = series(1/(1-x), x, n=10, method='symengine')
     assert ex.coeff(x,9) == 1
