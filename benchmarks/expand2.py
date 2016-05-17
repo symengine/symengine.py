bdffa35c3cb0229c986441b2ec9c2c12a8c7d0c4
Isuru Fernando <isuruf@gmail.com>
2016-05-17 22:51:23 +0530

    Add new benchmarks

--- benchmarks/expand2.py
+++ benchmarks/expand2.py
@@ -1,12 +1,22 @@
+#!/usr/bin/env python
+
 import sys
 sys.path.append("..")
+import os
 from timeit import default_timer as clock
-from csympy import var
-var("x y z w")
-e = (x+y+z+w)**15
-f = e*(e+w)
-print f
-t1 = clock()
-g = f.expand()
-t2 = clock()
-print "Total time:", t2-t1, "s"
+if os.environ.get("USE_SYMENGINE"):
+    from symengine import var
+else:
+    from sympy import var
+
+def main():
+    var("x y z w")
+    e = (x + y + z + w)**7
+    f = e * (e + w)
+    t1 = clock()
+    g = f.expand()
+    t2 = clock()
+    print("%s ms" % (1000 * (t2 - t1)))
+
+if __name__ == '__main__':
+  main()
