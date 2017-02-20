2f1ad955dc56c770dc245ca248796378e4d607d9
Isuru Fernando <isuruf@gmail.com>
2017-02-20 22:02:39 +0530

    Add tests for sympy_compat

--- symengine/tests/test_sympy_compat.py
+++ symengine/tests/test_sympy_compat.py
@@ -1,10 +1,11 @@
 from symengine.sympy_compat import (Integer, Rational, S, Basic, Add, Mul,
-    Pow, symbols, Symbol, log, sin, zeros, atan2)
+    Pow, symbols, Symbol, log, sin, zeros, atan2, Number)
 
 def test_Integer():
     i = Integer(5)
     assert isinstance(i, Integer)
     assert isinstance(i, Rational)
+    assert isinstance(i, Number)
     assert isinstance(i, Basic)
     assert i.p == 5
     assert i.q == 1
@@ -12,9 +13,13 @@ def test_Integer():
 def test_Rational():
     i = S(1)/2
     assert isinstance(i, Rational)
+    assert isinstance(i, Number)
     assert isinstance(i, Basic)
     assert i.p == 1
     assert i.q == 2
+    x = symbols("x")
+    assert not isinstance(x, Rational)
+    assert not isinstance(x, Number)
 
 def test_Add():
     x, y = symbols("x y")
@@ -67,3 +72,21 @@ def test_zeros():
 def test_has_functions_module():
     import symengine.sympy_compat as sp
     assert sp.functions.sin(0) == 0
+
+def subclass_symbol():
+    # Subclass of Symbol with an extra attribute
+    class Wrapper(Symbol):
+        def __new__(cls, name, extra_attribute):
+            return Symbol.__new__(cls, name)
+
+        def __init__(self, name, extra_attribute):
+            super(Wrapper, self).__init__(name)
+            self.extra_attribute = extra_attribute
+
+    # Instantiate the subclass
+    x = Wrapper("x", extra_attribute=3)
+    assert x.extra_attribute == 3
+    two_x = 2 * x
+    # Check that after arithmetic, same subclass is returned
+    assert two_x.args[1] is x
+
