29424022830759d19991cee38d8e4447d1aa6008
Isuru Fernando <isuruf@gmail.com>
2017-03-27 17:13:29 +0530

    Clean up travis-ci testing and use sage from conda

--- bin/install_travis.sh
+++ bin/install_travis.sh
@@ -1,15 +1,12 @@
 #!/usr/bin/env bash
 
-if [[ "${WITH_SAGE}" != "yes" ]]; then
-    # symengine's bin/install_travis.sh will install miniconda
-    conda_pkgs="python=${PYTHON_VERSION} pip cython sympy nose pytest"
-    if [[ "${WITH_NUMPY}" == "yes" ]]; then
-        conda_pkgs="${conda_pkgs} numpy";
-    fi
-else
-    wget -O- https://dl.dropboxusercontent.com/u/46807346/sage-6.9-x86_64-Linux-Ubuntu_12.04_64_bit.tar.gz | tar xz
-    SAGE_ROOT=`pwd`/sage-6.9-x86_64-Linux
-    export PATH="$SAGE_ROOT:$PATH"
-    source $SAGE_ROOT/src/bin/sage-env
-    export our_install_dir=$SAGE_LOCAL
+# symengine's bin/install_travis.sh will install miniconda
+export conda_pkgs="python=${PYTHON_VERSION} pip cython sympy nose pytest"
+
+if [[ "${WITH_NUMPY}" == "yes" ]]; then
+    export conda_pkgs="${conda_pkgs} numpy";
+fi
+
+if [[ "${WITH_SAGE}" == "yes" ]]; then
+    export conda_pkgs="${conda_pkgs} sage";
 fi
