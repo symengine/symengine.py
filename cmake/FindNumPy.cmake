c52d52abe43147860501f3305467ba9fe5210793
Björn Dahlgren <bjodah@gmail.com>
2017-06-17 19:38:34 +0200

    Fix FindNumPy.cmake

--- cmake/FindNumPy.cmake
+++ cmake/FindNumPy.cmake
@@ -9,9 +9,9 @@ if(NOT NUMPY_FIND_RESULT MATCHES 0)
   set(NUMPY_FOUND FALSE)
   message(STATUS "NumPy import failure:\n${NUMPY_FIND_ERROR}")  
 else()
-  find_path(NUMPY_INCLUDE_DIR numpy/arrayobject.h
-    HINTS "${__numpy_path}" "${NUMPY_FIND_OUTPUT}" NO_DEFAULT_PATH)
-  if(NUMPY_INCLUDE_DIR)
+  find_path(NUMPY_INCLUDE_PATH numpy/arrayobject.h
+    HINTS "${NUMPY_FIND_OUTPUT}" NO_DEFAULT_PATH)
+  if(NUMPY_INCLUDE_PATH)
     set(NUMPY_FOUND TRUE CACHE BOOL INTERNAL "NumPy found")
     INCLUDE(FindPackageHandleStandardArgs)
     FIND_PACKAGE_HANDLE_STANDARD_ARGS(NUMPY DEFAULT_MSG NUMPY_INCLUDE_PATH)
