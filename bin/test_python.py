52875a60820090eb26f69959c7dc34521a7e5b31
Shikhar Jaiswal <jaiswalshikhar87@gmail.com>
2017-09-30 15:27:33 +0530

    Corrections

--- bin/test_python.py
+++ bin/test_python.py
@@ -5,17 +5,12 @@ import symengine
 if not symengine.test():
     raise Exception('Tests failed')
 
-try:
+if TEST_SYMPY:
     import sympy
     from sympy.core.cache import clear_cache
     import atexit
 
     atexit.register(clear_cache)
-    have_sympy = True
-except ImportError:
-    have_sympy = False
-
-if TEST_SYMPY and have_sympy:
     print('Testing SYMPY')
     if not sympy.test('sympy/physics/mechanics'):
         raise Exception('Tests failed')
