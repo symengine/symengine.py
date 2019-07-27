68d64920b6080b61dfa1ab1dccc520cf783dabef
Isuru Fernando <isuruf@gmail.com>
2019-07-27 13:28:59 -0500

    Don't capture

--- bin/test_travis.sh
+++ bin/test_travis.sh
@@ -9,7 +9,7 @@ set -x
 python setup.py install build_ext --inplace --symengine-dir=$our_install_dir
 
 # Test python wrappers
-py.test -v $PYTHON_SOURCE_DIR/symengine/tests/test_*.py
+py.test -s -v $PYTHON_SOURCE_DIR/symengine/tests/test_*.py
 mkdir -p empty && cd empty
 python $PYTHON_SOURCE_DIR/bin/test_python.py
 cd ..
