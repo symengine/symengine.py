0fbf6f6d6ee304059054646dd0110bf2b7e8f981
Jialin Ma <marlin@inventati.org>
2020-05-04 01:03:16 -0400

    Add assign_to_pycapsule

--- symengine/lib/symengine_wrapper.pyx
+++ symengine/lib/symengine_wrapper.pyx
@@ -45,6 +45,11 @@ cpdef object sympify_pycapsule(object cap):
     p = <CRCPBasic*>PyCapsule_GetPointer(cap, NULL)
     return c2py(p.m)
 
+cpdef void assign_to_pycapsule(object x, object value):
+    cdef CRCPBasic *p_x
+    p_x = <CRCPBasic*>PyCapsule_GetPointer(cap, NULL)
+    p_x.m = sympify(value).thisptr
+
 cdef object c2py(rcp_const_basic o):
     cdef Basic r
     if (symengine.is_a_Add(deref(o))):
