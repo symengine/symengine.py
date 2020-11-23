a68f7fc95b4c39a480b09b79159372c58269e100
Rikard Nordgren <rikard.nordgren@farmaci.uu.se>
2020-11-23 21:58:27 +0100

    Export singleton sets from S
    Co-authored-by: Isuru Fernando <isuruf@gmail.com>

--- symengine/tests/test_solve.py
+++ symengine/tests/test_solve.py
@@ -7,10 +7,10 @@ def test_solve():
     x = Symbol("x")
     reals = Interval(-oo, oo)
 
-    assert solve(1, x, reals) == EmptySet()
+    assert solve(1, x, reals) == EmptySet
     assert solve(0, x, reals) == reals
     assert solve(x + 3, x, reals) == FiniteSet(-3)
-    assert solve(x + 3, x, Interval(0, oo)) == EmptySet()
+    assert solve(x + 3, x, Interval(0, oo)) == EmptySet
     assert solve(x, x, reals) == FiniteSet(0)
     assert solve(x**2 + 1, x) == FiniteSet(-I, I)
     assert solve(x**2 - 2*x + 1, x) == FiniteSet(1)
