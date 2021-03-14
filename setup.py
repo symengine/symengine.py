4ffaefeafebb279afa2930436b48c88a1ef3de41
Isuru Fernando <isuruf@gmail.com>
2021-03-13 19:39:08 -0600

    Update classifiers

--- setup.py
+++ setup.py
@@ -238,10 +238,9 @@ setup(name="symengine",
         'Topic :: Scientific/Engineering',
         'Topic :: Scientific/Engineering :: Mathematics',
         'Topic :: Scientific/Engineering :: Physics',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         ]
       )
