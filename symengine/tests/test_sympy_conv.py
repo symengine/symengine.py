803d00bd70f856828832bc664c16c25587430751
Rikard Nordgren <rikard.nordgren@farmaci.uu.se>
2021-03-29 20:31:43 +0200

    Update symengine/tests/test_sympy_conv.py
    Co-authored-by: Isuru Fernando <isuruf@gmail.com>

--- symengine/tests/test_sympy_conv.py
+++ symengine/tests/test_sympy_conv.py
@@ -790,9 +790,5 @@ def test_construct_dense_matrix():
     # Test for issue #347
     A = sympy.Matrix([[1, 2], [3, 5]])
     B = DenseMatrix(A)
-    assert B.rows == 2
-    assert B.cols == 2
-    assert B[0, 0] == 1
-    assert B[0, 1] == 2
-    assert B[1, 0] == 3
-    assert B[1, 1] == 5
+    assert B.shape == (2, 2)
+    assert list(B) == [1, 2, 3, 5]
