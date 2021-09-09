8b8ed55cca796669f08a5637a23c8de7f99acc8a
Isuru Fernando <isuruf@gmail.com>
2021-09-09 12:23:46 -0500

    check sdist

--- bin/test_travis.sh
+++ bin/test_travis.sh
@@ -5,6 +5,12 @@ set -e
 # Echo each command
 set -x
 
+python setup.py sdist
+mkdir dist-extract
+cd dist-extract
+tar -xvf ../dist/symengine-*.tar.gz
+cd symengine-*
+
 # Build inplace so that nosetests can be run inside source directory
 python3 setup.py install build_ext --inplace --symengine-dir=$our_install_dir
 
@@ -13,10 +19,3 @@ python3 -m pip install pytest
 python3 -m pytest -s -v $PYTHON_SOURCE_DIR/symengine/tests/test_*.py
 mkdir -p empty && cd empty
 python3 $PYTHON_SOURCE_DIR/bin/test_python.py
-cd ..
-
-if [[ "${TRIGGER_FEEDSTOCK}" == "yes" ]]; then
-    cd $PYTHON_SOURCE_DIR
-    ./bin/trigger_feedstock.sh
-fi
-
