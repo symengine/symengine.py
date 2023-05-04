0c1111c720829afea5d03b9b3f1ff7aec374d0f2
Isuru Fernando <isuruf@gmail.com>
2023-05-04 18:07:24 -0500

    Fix tests

--- symengine/tests/test_expr.py
+++ symengine/tests/test_expr.py
@@ -18,10 +18,11 @@ def test_as_powers_dict():
     x = Symbol('x')
     y = Symbol('y')
 
-    assert (2*x**y).as_coefficients_dict() == {2: 1, x: y}
-    assert (2*x**2*y**3).as_coefficients_dict() == {2: 1, x: 2, y: 3}
-    assert (-oo).as_coefficients_dict() == {-1: 1, oo: 1}
-    assert (x**y).as_coefficients_dict() == {x: y}
-    assert ((1/Integer(2))**y).as_coefficients_dict() == {2: -y}
-    assert (2**y).as_coefficients_dict() == {2: y}
-    assert (2**-y).as_coefficients_dict() == {2: -y}
+    assert (2*x**y).as_powers_dict() == {2: 1, x: y}
+    assert (2*x**2*y**3).as_powers_dict() == {2: 1, x: 2, y: 3}
+    assert (-oo).as_powers_dict() == {Integer(-1): 1, oo: 1}
+    assert (x**y).as_powers_dict() == {x: y}
+    assert ((1/Integer(2))**y).as_powers_dict() == {Integer(2): -y}
+    assert (x*(1/Integer(2))**y).as_powers_dict() == {x: Integer(1), Integer(2): -y}
+    assert (2**y).as_powers_dict() == {2: y}
+    assert (2**-y).as_powers_dict() == {2: -y}
